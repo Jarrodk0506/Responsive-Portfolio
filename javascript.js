686f2cfee40dcd6192a61079bcd5a190aa5248b7
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-08-21 14:02:01 -0400

    Made portfolio pictures into buttons to link to each respective project.

--- javascript.js
+++ javascript.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
 
     $(".submit").on("click", function() {
 
-        console.log("Hello");
+        console.log("Capture");
         var fName = document.getElementById('fname').value;
         var lName = document.getElementById('lname').value;
         var email = document.getElementById('email').value;
         var message = document.getElementById('subject').value;
        
         var divider = "\n------------------------------------------------------------\n\n";
-        var info = "\nFirst Name: " + fName + "\nLast Name: " + lName + "\nEmail: " + email + "\nMessage: " + message;
+        var info = "\nFirst Name: " + fName + "\nLast Name: " + lName + "\nEmail: " + email + "\nMessage: " + message + divider;
         console.log(info);
 
     //     fs.appendFile("log.txt", info + divider, function(err) {
@@ -19,8 +19,8 @@ $(document).ready(function() {
     //     console.log(info);
     //   });
 
-
     });
+
 
 });
 
