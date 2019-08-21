2a8fde935d2d7d844bd1178efb757ec19d9d9f49
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-08-20 21:56:05 -0400

    Update layout. Add captures on submit form. add javascript file for functionality

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// const fs = require('fs');
+
+$(document).ready(function() {
+
+    $(".submit").on("click", function() {
+
+        console.log("Hello");
+        var fName = document.getElementById('fname').value;
+        var lName = document.getElementById('lname').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('subject').value;
+       
+        var divider = "\n------------------------------------------------------------\n\n";
+        var info = "\nFirst Name: " + fName + "\nLast Name: " + lName + "\nEmail: " + email + "\nMessage: " + message;
+        console.log(info);
+
+    //     fs.appendFile("log.txt", info + divider, function(err) {
+    //     if (err) throw err;
+    //     console.log(info);
+    //   });
+
+
+    });
+
+});
+
+
+
+
+
+
+
